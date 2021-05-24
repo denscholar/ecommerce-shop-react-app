b9af5fa757e366a780123bfa0351547c52b7cb4e
Dennis Akagha <d.akagha20@gmail.com>
2021-05-24 12:26:36 +0100

    broken our homepage components into smaller components. finished styling our menu-item components.

---
+++ src/components/MenuItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './MenuItem.css';
+
+const MenuItem = ({ title, imageUrl, size }) => {
+    return(
+    <div
+        className={`${size} menu-item`}>
+        <div className='backgroundImage'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className="content">
+            <h1 className='title'>{title}</h1>
+            <span className='subtitle'>Shop Now</span>
+        </div>
+    </div>
+    )
+}
+
+export default MenuItem
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import '../pages/HomePage.css';
+import Directory from '../components/directory/Directory'
+
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+           <Directory />
+        </div>
+    )
+}
+
+export default HomePage
